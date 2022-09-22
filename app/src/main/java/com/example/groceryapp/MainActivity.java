3c9719efabe65d9817e939208cbd499d9b032894
tim1245 <tim.axelssonsafstro@gmail.com>
2022-09-22 15:28:34 +0200

    New UI only showing products, everything else removed

--- app/src/main/java/com/example/groceryapp/MainActivity.java
+++ app/src/main/java/com/example/groceryapp/MainActivity.java
@@ -14,57 +14,58 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText input;
-    private TextView textView;
+    RecyclerView recyclerView;
+    MainAdapter mainAdapter;
     // Get the database instance and store into object
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        input=findViewById(R.id.input);
-        Button btn = findViewById(R.id.btn);
-        Button btnRead = findViewById(R.id.btnRead);
-        textView = findViewById(R.id.textView);
-        Button show = findViewById(R.id.showProducts);
 
-        DatabaseReference rootDatabaseref = FirebaseDatabase.getInstance("https://grocery-price-tracker-fedd5-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Message");
-        DatabaseReference LidlDatabaseref = FirebaseDatabase.getInstance("https://grocery-price-tracker-fedd5-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Lidl");
-
-        btnRead.setOnClickListener(view -> LidlDatabaseref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
-                    String data=snapshot.getValue().toString();
-                    textView.setText(data);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        }));
-
-        show.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                OpenActivity();
-            }
-        });
-        btn.setOnClickListener(view -> {
-            String data=input.getText().toString();
-
-            rootDatabaseref.setValue(data);
-        });
+        recyclerView = findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
+        FirebaseRecyclerOptions<MainModel> options=
+                new FirebaseRecyclerOptions.Builder<MainModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Test"), MainModel.class)
+                        .build();
+
+        mainAdapter = new MainAdapter(options);
+        recyclerView.setAdapter(mainAdapter);
 
     }
-    public void OpenActivity(){
-        Intent intent = new Intent(this, showProducts.class);
-        startActivity(intent);
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mainAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainAdapter.stopListening();
     }
 }
