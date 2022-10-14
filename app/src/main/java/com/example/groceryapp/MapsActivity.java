efaac739f2b46e5c3535dfac6405d91d4407e364
MadianAlsabbagh <madian.alsabbagh@gmail.com>
2022-10-14 19:34:08 +0200

    The map now shows current location and the closest store

--- app/src/main/java/com/example/groceryapp/MapsActivity.java
+++ app/src/main/java/com/example/groceryapp/MapsActivity.java
@@ -87,22 +87,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         coopBergvikLoc.setLatitude(59.377955);
         coopBergvikLoc.setLongitude(13.425271);
 
-        Location icaValsvikenLoc=new Location("Ica Valsviken");
+        Location coopValsvikenLoc=new Location("Coop Välsviken");
+        coopValsvikenLoc.setLatitude(59.39631609438561);
+        coopValsvikenLoc.setLongitude(13.575848584939761);
+
+        Location icaValsvikenLoc=new Location("Ica Välsviken");
         icaValsvikenLoc.setLatitude(59.39636019797852);
         icaValsvikenLoc.setLongitude(13.580784956104168);
+
+        Location icaBergvikLoc=new Location("Ica Bergvik");
+        icaBergvikLoc.setLatitude(59.37816709297543);
+        icaBergvikLoc.setLongitude(13.429414902125185);
 
         Location willysCityLoc=new Location("Willys City");
         willysCityLoc.setLatitude(59.37873877071142);
         willysCityLoc.setLongitude(13.509179240196127);
 
-        Location lidlLoc=new Location("Lidl Våxnäs");
-        lidlLoc.setLatitude(59.38695327943067);
-        lidlLoc.setLongitude(13.47556768120986);
+        Location willysVaxnasLoc=new Location("Willys Våxnäs");
+        willysVaxnasLoc.setLatitude(59.39007983348828);
+        willysVaxnasLoc.setLongitude(13.481656116945008);
+
+        Location lidlVaxnasLoc=new Location("Lidl Våxnäs");
+        lidlVaxnasLoc.setLatitude(59.38695327943067);
+        lidlVaxnasLoc.setLongitude(13.47556768120986);
+
+        Location lidlKroppkarrLoc=new Location("Lidl Kroppkärr");
+        lidlKroppkarrLoc.setLatitude(59.39815733970923);
+        lidlKroppkarrLoc.setLongitude(13.535097127236481);
+
+
 
         locations.add(coopBergvikLoc);
+        locations.add(coopValsvikenLoc);
         locations.add(icaValsvikenLoc);
+        locations.add(icaBergvikLoc);
         locations.add(willysCityLoc);
-        locations.add(lidlLoc);
+        locations.add(willysVaxnasLoc);
+        locations.add(lidlVaxnasLoc);
+        locations.add(lidlKroppkarrLoc);
+
 
 
 
@@ -113,27 +136,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng icaValsviken = new LatLng(59.39636019797852, 13.580784956104168);
         LatLng willysCity = new LatLng(59.37873877071142, 13.509179240196127);
         LatLng willysVaxnas = new LatLng(59.39007983348828, 13.481656116945008);
-        LatLng icaSuperMarketWallinder = new LatLng(59.39483129478293, 13.517329998415354);
-        LatLng icaSuperMarketHaga = new LatLng(59.38535399115744, 13.515270971750242);
-        LatLng icaViken = new LatLng(59.37790211919998, 13.490859116915285);
-        LatLng icaKarnan = new LatLng(59.387091362830496, 13.46403259886529);
-        LatLng icaSuperMarketFanfar = new LatLng(59.3851325779657, 13.48540154747029);
         LatLng icaBergvik = new LatLng(59.37816709297543, 13.429414902125185);
-        LatLng lidl = new LatLng(59.38695327943067, 13.47556768120986);
+        LatLng lidlVaxnas = new LatLng(59.38695327943067, 13.47556768120986);
+        LatLng lidlKroppkarr = new LatLng(59.39815733970923, 13.535097127236481);
+
+        //LatLng icaSuperMarketWallinder = new LatLng(59.39483129478293, 13.517329998415354);
+        //LatLng icaSuperMarketHaga = new LatLng(59.38535399115744, 13.515270971750242);
+        //LatLng icaViken = new LatLng(59.37790211919998, 13.490859116915285);
+        //LatLng icaKarnan = new LatLng(59.387091362830496, 13.46403259886529);
+        //LatLng icaSuperMarketFanfar = new LatLng(59.3851325779657, 13.48540154747029);
+
 
         mMap.addMarker(new MarkerOptions().position(coopBergvik).title("Stora Coop Bergvik"));
         mMap.addMarker(new MarkerOptions().position(coopValsviken).title("Stora Coop Välsviken"));
         mMap.addMarker(new MarkerOptions().position(icaValsviken).title("Ica-Maxi Välsviken"));
         mMap.addMarker(new MarkerOptions().position(willysCity).title("Willys City"));
         mMap.addMarker(new MarkerOptions().position(willysVaxnas).title("Willys Våxnäs Industriområde"));
-        mMap.addMarker(new MarkerOptions().position(icaSuperMarketWallinder).title("ICA Supermarket Wallinders"));
+        mMap.addMarker(new MarkerOptions().position(icaBergvik).title("Ica-Maxi Bergvik"));
+        mMap.addMarker(new MarkerOptions().position(lidlVaxnas).title("Lidl Våxnäs"));
+        mMap.addMarker(new MarkerOptions().position(lidlKroppkarr).title("Lidl Kroppkärr"));
+        /*mMap.addMarker(new MarkerOptions().position(icaSuperMarketWallinder).title("ICA Supermarket Wallinders"));
         mMap.addMarker(new MarkerOptions().position(icaSuperMarketHaga).title("Ica Supermarket Hagahallen"));
         mMap.addMarker(new MarkerOptions().position(icaViken).title("Ica Nära Viken"));
         mMap.addMarker(new MarkerOptions().position(icaKarnan).title("Ica Nära Kärnan"));
-        mMap.addMarker(new MarkerOptions().position(icaSuperMarketFanfar).title("Ica Supermarket Fanfaren"));
-        mMap.addMarker(new MarkerOptions().position(icaBergvik).title("Ica-Maxi Bergviken"));
-        mMap.addMarker(new MarkerOptions().position(lidl).title("Lidl Våxnäs"));
-
+        mMap.addMarker(new MarkerOptions().position(icaSuperMarketFanfar).title("Ica Supermarket Fanfaren"));*/
         mMap.moveCamera(CameraUpdateFactory.newLatLng(karlstad));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(karlstad, 11));
         getCurrentLocation();
@@ -155,9 +181,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onSuccess(Location location) {
                 if (location != null) {
                     currentLocation = location;
-
-
-
 
                     LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                     MarkerOptions current = new MarkerOptions().position(latLng).title("Current location");
