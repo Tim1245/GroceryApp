48c78bf6d3593e4a5fb4264da168e2d5602394d2
Linus <linusnyflott@gmail.com>
2022-10-19 14:28:51 +0200

    Moved creating the layout from onCreate to onResume. This means that the app automatically changes language when a page is displayed again, meaning we don't require users to use the specific back button we programmed.

--- app/src/main/java/com/example/groceryapp/MainActivity.java
+++ app/src/main/java/com/example/groceryapp/MainActivity.java
@@ -37,8 +37,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // If we want to force immediate login
-        //UserManagement.RequireUserLogin(this);
+        /*localehelper.onMake(this);
+        setContentView(R.layout.newhomepage);
+        ImageView btnRead = findViewById(R.id.productbtn);
+        ImageView btnMap = findViewById(R.id.mapbtn);
+        ImageView btnLogOut = findViewById(R.id.logoutbtn);
+        ImageView btnSettings = findViewById(R.id.settingbtn);
+        ImageView btnSearch = findViewById(R.id.searchbtn);
+
+
+        btnMap.setOnClickListener(view -> OpenActivity(MapsActivity.class));
+
+        btnRead.setOnClickListener(view -> OpenActivity(ButikerActivity.class));
+
+        btnLogOut.setOnClickListener((View view) ->  {
+            UserManagement.signOut();
+            if (LoginActivity.logOutPressed == 1)
+                Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
+            LoginActivity.logOutPressed = 0;
+        });
+
+        btnSettings.setOnClickListener(view -> OpenActivity(Settings.class));
+
+        btnSearch.setOnClickListener(view -> {
+
+            Intent intent = new Intent(MainActivity.this, ReadDatabase.class);
+            intent.putExtra("Butik", "ALL");
+            startActivity(intent);
+        });
+
+        startService(new Intent(this, NotificationHandler.class));
+*/
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
         localehelper.onMake(this);
         setContentView(R.layout.newhomepage);
         ImageView btnRead = findViewById(R.id.productbtn);
--- app/src/main/java/com/example/groceryapp/Settings.java
+++ app/src/main/java/com/example/groceryapp/Settings.java
@@ -6,6 +6,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,7 +23,7 @@ public class Settings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+/*
         TextView messageView;
         Context context;
         Resources resources;
@@ -67,7 +68,52 @@ public class Settings extends AppCompatActivity {
         });
 
 
+*/
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("TEMP DEBUG", "Calling on resume");
+        setContentView(R.layout.settings);
+        ImageView faqbtn = findViewById(R.id.FAQbtn);
+        TextView faqtext = findViewById(R.id.faqtext);
+        ImageView languaguebtn = findViewById(R.id.languagebtn);
+        TextView backbtn = findViewById(R.id.textView13);
+        ImageView btnback = findViewById(R.id.left);
+        LocaleHelper localhelper = new LocaleHelper();
+
+
+        faqbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenActivity(FAQactivity.class);
+            }
+        });
+        languaguebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenActivity(LanguageSettings.class);
+            }
+        });
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenActivity(MainActivity.class);
+            }
+        });
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenActivity(MainActivity.class);
+            }
+        });
+        faqtext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenActivity(FAQactivity.class);
+            }
+        });
     }
 
     public void OpenActivity(Class activity){
